f7126ac7b97557236ed85d454fede7043a1d8769
张嘉旺 <zhangjiawang99@126.com>
2022-06-23 17:54:06 +0800

    发送邮件验证码

--- routes/api.go
+++ routes/api.go
@@ -27,6 +27,7 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			authGroup.POST("/verify-codes/captcha", vcc.ShowCaptcha)
 
 			authGroup.POST("/verify-codes/phone", vcc.SendUsingPhone)
+			authGroup.POST("/verify-codes/email", vcc.SendUsingEmail)
 		}
 	}
 }
