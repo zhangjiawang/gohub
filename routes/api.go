0cd1aff770b4cd0460d1e85647996031f9663121
张嘉旺 <zhangjiawang99@126.com>
2022-06-23 18:24:50 +0800

    手机号+短信验证码注册

--- routes/api.go
+++ routes/api.go
@@ -20,13 +20,17 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
 			// 判断 Email 是否已注册
 			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
+			// 使用手机注册
+			authGroup.POST("/signup/using-phone", suc.SignupUsingPhone)
 
 			// 发送验证码
 			vcc := new(auth.VerifyCodeController)
 			// 图片验证码，需要加限流
 			authGroup.POST("/verify-codes/captcha", vcc.ShowCaptcha)
 
+			// 发送短信验证码
 			authGroup.POST("/verify-codes/phone", vcc.SendUsingPhone)
+			// 发送邮件验证码
 			authGroup.POST("/verify-codes/email", vcc.SendUsingEmail)
 		}
 	}
