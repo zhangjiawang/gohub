b015ed09f56a7c9d061d63fe8a78b39cf82308f1
张嘉旺 <zhangjiawang99@126.com>
2022-06-24 10:16:14 +0800

    通过手机找回密码

--- routes/api.go
+++ routes/api.go
@@ -42,6 +42,9 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			authGroup.POST("/login/using-password", lgc.LoginByPassword)
 			// 刷新 token
 			authGroup.POST("/login/refresh-token", lgc.RefreshToken)
+			// 重置密码
+			pwc := new(auth.PasswordController)
+			authGroup.POST("/password-reset/using-phone", pwc.ResetByPhone)
 		}
 	}
 }
